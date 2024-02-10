5dc4d2da6cf0db0b9e76aafd82aa71072b145dad
kammatysiak <156349768+kammatysiak@users.noreply.github.com>
2024-02-10 11:11:10 +0100

    Revert "+ MED-09 (#1)"
    This reverts commit 2a8f6cdf16e56ad18ba11b71ed36774aad45da00.

--- src/main/java/com/kammatysiak/medicalclinic/repository/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kammatysiak.medicalclinic.repository;
-
-import com.kammatysiak.medicalclinic.model.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, String> {
-
-}
